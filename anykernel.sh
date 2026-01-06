bab75727799901b207d429c86b10505db16c12a3
RohmanU <67408676+rohmanurip@users.noreply.github.com>
2026-01-06 14:26:22 +0700

    set kernel string

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Lunar Kernel by Kenskuyy
+kernel.string=Perf+ Kernel by rohmanurip
 do.devicecheck=1
 do.modules=0
 do.systemless=1
